5254dbccab566bcb512d9bfff87a7c29c4ee2d21
DesignedByQ <henry.o@hotmail.co.uk>
2023-06-22 15:32:59 +0100

    customer acc reg page working

--- src/Signup.js
+++ src/Signup.js
@@ -2,54 +2,294 @@ import React from 'react'
 import { Link } from 'react-router-dom';
 import Validation from './SignupValidation';
 import { useState } from 'react';
+//import { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 //import axios from 'axios';
-
 
 function Signup() {
 
     const [values, setValues] = useState({
-        name: '',
+        account: '',
+        username: '',
+        firstname: '',
+        middlename: '',
+        dob: '',
+        lastname: '',
+        mobile: '',
+        addressDTO: {buildingNo: '', firstLine: '', secondLine: '', city: '', county: '', postCode: ''},
+        fileDTO: {fileName: '', fileType: '', fileData: ''},
         email: '',
-        password: ''
+        password: '',
+        confirmpassword: '',
+        approved: false,
+        verdict: "TBC",
+        
     })
 
-    const [errors, setErrors] = useState({})
+    const navigate = useNavigate();
+    const [errors, setErrors] = useState({});
+    const [isLoading, setIsLoading] = useState(false);
+    const [isError, setIsError] = useState(false);
 
     const handleInput = (event) => {
-        setValues(prev => ({...prev, [event.target.name]: [event.target.value]}));
+
+        const { name, value, files } = event.target;
+
+        if (name.includes('.')) {
+
+            const [parent, child] = name.split('.');
+
+            setValues((prev) => ({...prev, [parent]: { ...prev[parent], [child]: value }}));
+        
+        } else {
+
+            setValues((prev) => ({
+                ...prev, [name]: value
+            }));
+
+        }
+        // console.log(values.fileDTO.fileType)
+        // console.log(values.fileDTO.fileName)
+        // console.log(values.fileDTO.fileData)
+        // const fileExtension = values.fileDTO.fileData.split('.').pop().toLowerCase()
+        // values.fileDTO.fileType = `"image/${fileExtension}"`
+        
+        // const fname = values.fileDTO.fileData.split('\\').pop().toLowerCase();
+        // values.fileDTO.fileName = fname.split('.').shift().toLowerCase();
+        // console.log(values.fileDTO.fileName)
+        if (files && files[0]) {
+            const reader = new FileReader();
+            
+            // const fname = values.fileDTO.fileData.split('\\').pop().toLowerCase();
+            // values.fileDTO.fileName = fname.split('.').shift().toLowerCase();
+            // console.log(1111)
+            // console.log(files[0])
+            // console.log(files[0].name)
+            reader.onload = () => {
+                const base64Image = reader.result;
+
+                const base64ImageData = base64Image.split(',').pop()
+
+                setValues((prev) => ({
+                    ...prev,
+                    fileDTO: { ...prev.fileDTO, fileData: base64ImageData, fileType: files[0].type, fileName: files[0].name },
+                    
+                }));
+            };
+
+            reader.readAsDataURL(files[0]);
+
+        }
+        // Read the image file (assuming you have access to the file object)
+        // const fileReader = new FileReader();
+        // fileReader.noload = function (event) {
+        //     const base64Image = event.target.result
+
+        //     values.fileDTO.fileData = base64Image
+        // }
+
+        //fileReader.readAsDataURL(`${values.fileDTO.fileData}`)
 
     }
 
-    const handleSubmit = (event) => {
+    const url = `http://localhost:8081/api/uploadcust`;
+
+    const handleSubmit = async (event) => {
         event.preventDefault();
         setErrors(Validation(values));
         // if(errors.name === "" && errors.email === "" && errors.password === "") {
         //     axios.post('')
         // }
-    }
+        
+        if(Object.keys(errors).length === 0) {
+
+            setIsLoading(true);
+            setIsError(false);
+
+
+
+            //console.log(values.fileDTO.fileData)
+
+            try {
+                //console.log(JSON.stringify(values))
+                const response = await fetch(url, {
+                  method: 'POST',
+                  headers: {
+      
+                      'Content-Type': 'application/json',
+      
+                  },
+                  
+                  body: JSON.stringify(values)
+                  
+                });
+        
+                if (!response.ok) {
+                  
+                  setIsError(true);
+                  
+                } else {
+
+                   
+                    //do get req on submitted page
+                  // this page should navigate to a details succeffully submitted page. check your emails within the next 24hrs for a decsion on your account
+                  navigate('/twofacode');
+        
+                }
+        
+              } catch (error) {
+        
+                setIsError(true);
+                console.log(error);
+        
+              }
+        
+              setIsLoading(false);
+
+            }
+
+        };
     
   return (
     <div className="">
         <div>
-            <h2>Sign-Up</h2>
+        {isLoading && <h2>Submitting details...</h2>}
+        {isError && <h2>There has been an error with submitting your details, please try again later</h2>}
+        <h2>Sign-Up</h2>
+
+            <h2>Enter your details for your new account.</h2>
             <form action="" onSubmit={handleSubmit}>
                 <div>
-                    <label htmlFor="name">Name</label>
-                    <input type="text" placeholder="Enter Name" name="name" 
-                    onChange={handleInput} />
-                    {errors.name && <span>{errors.name}</span>}
+                    <fieldset>
+                        <legend>Select which account you would like to open:</legend>
+                        {errors.account && <span>{errors.account}</span>}
+                        <div>
+                            <input type="radio" id="current" name="account" value="current" checked={values.account === "current"} onChange={handleInput}/>
+                            <label htmlFor="current">Current Account</label>
+                        </div>
+
+                        <div>
+                            <input type="radio" id="savings" name="account" value="savings" checked={values.account === "savings"} onChange={handleInput}/>
+                            <label htmlFor="savings">Savings Account</label>
+                        </div>
+
+                        <div>
+                            <input type="radio" id="creditcard" name="account" value="creditcard" checked={values.account === "creditcard"} onChange={handleInput}/>
+                            <label htmlFor="creditcard">Credit Card</label>
+                        </div>
+                    </fieldset>
                 </div>
                 <div>
-                    <label htmlFor="email">Email</label>
-                    <input type="email" placeholder="Enter Email" name="email" 
-                    onChange={handleInput} />
-                    {errors.email && <span>{errors.email}</span>}
+                    <small>Please insure all details provided match the identification document provided.</small>
                 </div>
                 <div>
-                    <label htmlFor="password">Password</label>
-                    <input type="password" placeholder="Enter Password" name="password" 
+                    <label htmlFor="username">User Name</label>
+                    <input type="text" placeholder="Enter Username" name="username" 
                     onChange={handleInput} />
-                    {errors.password && <span>{errors.password}</span>}
+                    {errors.username && <span>{errors.username}</span>}
+                </div>
+                <div>
+                    <label htmlFor="firstname">First Name</label>
+                    <input type="text" placeholder="Enter First Name" name="firstname" 
+                    onChange={handleInput} />
+                    {errors.firstname && <span>{errors.firstname}</span>}
+                </div>
+                <div>
+                    <label htmlFor="middlename">Middle Name</label>
+                    <input type="text" placeholder="Enter Middle Name" name="middlename" 
+                    onChange={handleInput} />
+                    {errors.middlename && <span>{errors.middlename}</span>}
+                </div>
+                <div>
+                    <label htmlFor="lastname">Last Name</label>
+                    <input type="text" placeholder="Enter Last Name" name="lastname" 
+                    onChange={handleInput} />
+                    {errors.lastname && <span>{errors.lastname}</span>}
+                </div>
+                <div>
+                    <label htmlFor="dob">Date of Birth</label>
+                    <input type="date" placeholder="Enter Last Name" name="dob" 
+                    onChange={handleInput} />
+                    {errors.dob && <span>{errors.dob}</span>}
+                </div>
+                <div>
+                    <label htmlFor="mobile">Mobile</label>
+                    <input type="text" placeholder="Enter Mobile Number" name="mobile" 
+                    onChange={handleInput} />
+                    {errors.mobile && <span>{errors.mobile}</span>}
+                </div>
+                <div>
+                    <label htmlFor="addressDTO.buildingNo">Building No.</label>
+                    <input type="text" placeholder="Enter Building Number" name="addressDTO.buildingNo" 
+                    onChange={handleInput} />
+                    {errors.buildingNo && <span>{errors.buildingNo}</span>}
+
+                    <label htmlFor="addressDTO.firstLine">First Line</label>
+                    <input type="text" placeholder="Enter First Line" name="addressDTO.firstLine" 
+                    onChange={handleInput} />
+                    {errors.firstLine && <span>{errors.firstLine}</span>}
+
+                    <label htmlFor="addressDTO.secondLine">Second Line</label>
+                    <input type="text" placeholder="Enter Second Line" name="addressDTO.secondLine" 
+                    onChange={handleInput} />
+                    {errors.secondLine && <span>{errors.secondLine}</span>}
+
+                    <label htmlFor="addressDTO.city">City</label>
+                    <input type="text" placeholder="Enter City" name="addressDTO.city" 
+                    onChange={handleInput} />
+                    {errors.city && <span>{errors.city}</span>}
+
+                    <label htmlFor="addressDTO.county">County</label>
+                    <input type="text" placeholder="Enter County" name="addressDTO.county" 
+                    onChange={handleInput} />
+                    {errors.county && <span>{errors.county}</span>}
+
+                    <label htmlFor="addressDTO.postCode">Post Code</label>
+                    <input type="text" placeholder="Enter Post Code" name="addressDTO.postCode" 
+                    onChange={handleInput} />
+                    {errors.postCode && <span>{errors.postCode}</span>}
+
+                    {/* <label htmlFor="country">Country</label>
+                    <input type="text" placeholder="Enter Country" name="country" 
+                    onChange={handleInput} />
+                    {errors.country && <span>{errors.country}</span>} */}
+                </div>
+                <div>
+                <h3>Proof of ID</h3>
+                <small>As part of your application we require you to submit one form of government issued ID</small>
+                {errors.fileDTO && <span>{errors.fileDTO}</span>}
+                <div>
+                    <label htmlFor="fileDTO.fileData">Identification Document</label>
+                    <input type='file' name='fileDTO.fileData' onChange={handleInput} />
+                </div>
+                
+                <small>File format must be either PDF, JPG, PNG only.</small>
+                
+                </div>
+                <div>
+                    <h3>New Login Details</h3>
+                    <div>
+                        <label htmlFor="email">Email</label>
+                        <input type="email" placeholder="Enter Email" name="email" 
+                        onChange={handleInput} />
+                        {errors.email && <span>{errors.email}</span>}
+                    </div>
+                    <div>
+                        <label htmlFor="password">Password</label>
+                        <input type="password" placeholder="Enter Password" name="password" 
+                        onChange={handleInput} />
+                        {errors.password && <span>{errors.password}</span>}
+
+                        <label htmlFor="confirmpassword">Confirm Your Password</label>
+                        <input type="password" placeholder="Enter Password Again" name="confirmpassword" 
+                        onChange={handleInput} />
+                        {errors.confirmpassword && <span>{errors.confirmpassword}</span>}
+
+                        
+                        <div>
+                            <small>Password must include one capital letter, 3 numbers and be at least 11 characters long.</small>
+                        </div>
+                    </div>
                 </div>
 
                 <button type="submit" className="">SIGN UP</button>
--- src/SignupValidation.js
+++ src/SignupValidation.js
@@ -1,14 +1,48 @@
 function Validation(values) {
     let error = {}
+
     const email_pattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/
     const password_pattern = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[a-zA-Z0-9]{8,}$/
+    const account_pattern = /(current|savings|creditcard)/
+    const username_pattern = /^[a-zA-Z0-9]{2,100}$/
+    const firstname_pattern = /^[a-zA-Z]{1,50}$/
+    const middlename_pattern = /^[a-zA-Z]{1,50}$/
+    const lastname_pattern = /^[a-zA-Z]{1,50}$/
+    const dob_pattern = /^\d{4}-\d{2}-\d{2}$/
+    const mobile_pattern = /^\d{11}$/
+    const buildingNo_pattern = /^[0-9]{1,5}$/
+    const firstLine_pattern = /^(?=.{1,100}$)[a-zA-Z]+(?: [a-zA-Z]+)?$/
+    const secondLine_pattern = /^(?=.{1,100}$)[a-zA-Z]+(?: [a-zA-Z]+)?$/
+    const city_pattern = /^(?=.{2,60}$)[a-zA-Z]+(?: [a-zA-Z]+)?$/
+    const county_pattern = /^(?=.{1,30}$)[a-zA-Z]+(?: [a-zA-Z]+)?$/
+    const postCode_pattern = /^[A-Z]{1,2}[0-9R][0-9A-Z]? [0-9][A-Z]{2}$/i
+    const confirmpassword = values.password
 
-    if(values.name === "") {
-        error.name = "Name should not be empty"
+
+    //console.log(values.fileDTO.fileData)
+    // Check if a file is selected
+    if (!values.fileDTO.fileData) {
+        error.fileDTO = 'Please select a file to upload for verification.';
     } else {
-        error.name = ""
-    }
 
+        // Get the file extension
+        //var fileName = values.file.name;
+        var fileExtension = values.fileDTO.fileData.split('.').pop().toLowerCase();
+        //console.log(fileExtension)
+        // Allowed file extensions
+        var allowedExtensions = ['pdf', 'jpg', 'jpeg', 'png'];
+
+        // Check if the file extension is allowed
+        if (!allowedExtensions.includes(fileExtension)) {
+            error.fileDTO = 'Only PDF, JPG, and PNG files are allowed.';
+        } else {
+
+            // Validation successful, allow the form submission
+            error.fileDTO = "";
+
+        }
+
+    }
 
     if(values.email === "") {
         error.email = "Email should not be empty"
@@ -24,6 +58,118 @@ function Validation(values) {
         error.password = "Password didn't match"
     } else {
         error.password = ""
+    }
+
+    if(values.account === "") {
+        error.account = "Please select an account type."
+    } else if(!account_pattern.test(values.account)) {
+        error.password = "The account type selected is invalid."
+    }else {
+        error.account = ""
+    }
+
+    if(values.username === "") {
+        error.username = "Username should not be empty"
+    } else if(!username_pattern.test(values.username)) {
+        error.username = "Username must be between 2-100 characters long and include no special characters or spaces."
+    } else {
+        error.username = ""
+    }
+
+    if(values.firstname === "") {
+        error.firstname = "First name should not be empty"
+    } else if(!firstname_pattern.test(values.firstname)) {
+        error.firstname = "First name should only contain alphabetic characters."
+    } else {
+        error.firstname = ""
+    }
+
+    if(values.middlename === "") {
+        error.middlename = "Middle name should not be empty"
+    } else if(!middlename_pattern.test(values.middlename)) {
+        error.middlename = "Middle name should only contain alphabetic characters."
+    } else {
+        error.middlename = ""
+    }
+
+    if(values.lastname === "") {
+        error.lastname = "Last name should not be empty"
+    } else if(!lastname_pattern.test(values.lastname)) {
+        error.lastname = "Last name should only contain alphabetic characters."
+    } else {
+        error.lastname = ""
+    }
+
+    if(values.dob === "") {
+        error.dob = "Date of birth should not be empty"
+    } else if(!dob_pattern.test(values.dob)) {
+        error.dob = "Date of birth should only contain alphabetic characters."
+    } else {
+        error.dob = ""
+    }
+
+    if(values.mobile === "") {
+        error.mobile = "Mobile should not be empty"
+    } else if(!mobile_pattern.test(values.mobile)) {
+        error.mobile = "Mobile should only contain numbers and should be 11 digits long."
+    } else {
+        error.mobile = ""
+    }
+
+    if(values.addressDTO.buildingNo === "") {
+        error.buildingNo = "Building No. should not be empty"
+    } else if(!buildingNo_pattern.test(values.addressDTO.buildingNo)) {
+        error.buildingNo = "Building No. should only contain numbers and be no more than 5 digits."
+    } else {
+        error.buildingNo = ""
+    }
+
+    if(values.addressDTO.firstLine === "") {
+        error.firstLine = "First line should not be empty"
+    } else if(!firstLine_pattern.test(values.addressDTO.firstLine)) {
+        error.firstLine = "First line should only contain letters and be no more than 100 characters."
+    } else {
+        error.firstLine = ""
+    }
+
+    if(values.addressDTO.secondLine === "") {
+        error.secondLine = "Second line should not be empty"
+    } else if(!secondLine_pattern.test(values.addressDTO.secondLine)) {
+        error.secondLine = "Second line should only contain letters and be no more than 100 characters."
+    } else {
+        error.secondLine = ""
+    }
+
+    if(values.addressDTO.city === "") {
+        error.city = "City should not be empty"
+    } else if(!city_pattern.test(values.addressDTO.city)) {
+        error.city = "City should only contain letters and be between 2-60 characters."
+    } else {
+        error.city = ""
+    }
+
+    if(values.addressDTO.county === "") {
+        error.county = "County should not be empty"
+    } else if(!county_pattern.test(values.addressDTO.county)) {
+        error.county = "County should only contain letters and be between 1-30 characters."
+    } else {
+        error.county = ""
+    }
+
+    if(values.addressDTO.postCode === "") {
+        error.postCode = "Post code should not be empty"
+    } else if(!postCode_pattern.test(values.addressDTO.postCode)) {
+        error.postCode = "Post code should only contain letters and numbers and be between 5-7 characters (excluding the space)."
+    } else {
+        error.postCode = ""
+    }
+
+    if(values.confirmpassword === "") {
+        error.confirmpassword = "Please confirm your password, it should not be empty."
+    } else if(confirmpassword !== values.password) {
+        error.confirmpassword = "Passwords provided do not match!"
+    } else {
+        error.confirmpassword = ""
     }
 
     return error;

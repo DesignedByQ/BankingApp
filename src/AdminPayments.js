858d3a42e0d577144a6d0d75025d231ffc7a210a
DesignedByQ <henry.o@hotmail.co.uk>
2023-07-20 00:31:04 +0100

    External transfer now working

--- src/AdminPayments.js
+++ src/AdminPayments.js
@@ -8,23 +8,30 @@ function AdminPayments() {
         amount: "",
         reference: "",
         accountId1: "",
-        sortCode1: ""
+        sortCode1: "",
+
+        //external
+        accountId3: "",
+        sortCode3: "",
+        accountId4: "",
+        sortCode4: "",
+        bank: "",
+        amount2: "",
+        reference1: ""
     });
 
     const [isTransferSuccessful, setIsTransferSuccessful] = useState(false);
     const [isTransferSuccessfulInt, setIsTransferSuccessfulInt] = useState(false);
+    const [isTransferSuccessfulExt, setIsTransferSuccessfulExt] = useState(false);
+
+    const [isError, setIsError] = useState(false);
 
     const handleSubmitAdd = async (e) => {
         e.preventDefault();
 
-        const longVal = parseInt(formData.accountId, 10)
-        //console.log(url)
-        console.log(formData.accountId)
-        console.log(typeof longVal)
-
         const add_url = `http://localhost:8083/api/updatebalance/${formData.accountId}`;
 
-        const url = `http://localhost:8083/api/getaccount/${longVal}`;
+        const url = `http://localhost:8083/api/getaccount/${formData.accountId}`;
 
         try {
 
@@ -33,11 +40,11 @@ function AdminPayments() {
             });
 
             if (!response.ok) {
-                //setIsError(true);
+                setIsError(true);
                 console.log("error is true")
             } else {
                 const data = await response.json();
-                //setFormData(data);
+                
                 console.log(data)
                 //check that the SC is the same if so add amount to balance and patch the balance
              
@@ -77,11 +84,11 @@ function AdminPayments() {
                     });
         
                     if (!response1.ok) {
-                        //setIsError(true);
+                        setIsError(true);
                         console.log("error is true")
                     } else {
                         const data1 = await response1.json();
-                        //setFormData(data);
+                        
                         console.log(data1)
                         handleClearForm()
 
@@ -91,18 +98,13 @@ function AdminPayments() {
             }
 
         } catch (error) {
-            //setIsError(true);
+            setIsError(true);
             console.log(error);
         }
     };
 
     const handleSubmitInt = async (e) => {
         e.preventDefault();
-        
-        //const acc1 = parseInt(formData.accountId)
-
-        //const acc2 = parseInt(formData.accountId1)
-
 
         const int_url = `http://localhost:8083/api/transfer/from/${formData.accountId}/to/${formData.accountId1}`;
 
@@ -130,10 +132,9 @@ function AdminPayments() {
 
             if(!response2.ok || !response22.ok){
 
-                //setIsError(true)
-                console.log(response2.error)
-                console.log(response22.error)
-
+                setIsError(true)
+                console.log("isError is " + isError)
+   
             } else {
 
                 const data2 = await response2.json();
@@ -143,15 +144,6 @@ function AdminPayments() {
                 console.log(data22)
 
                 if((parseInt(formData.sortCode) === data2.sortCode) && (parseInt(formData.sortCode1) === data22.sortCode)){
-
-                    // const doubleBal = Number(data.balance)
-
-                    // console.log(doubleBal)
-
-                    // const dataObject = {}
-
-
-                    //const input = parseInt(formData.amount)
 
                     const doubleBal = Number(formData.amount)
                     console.log(doubleBal)
@@ -178,7 +170,8 @@ function AdminPayments() {
 
                         if(!response3.ok){
 
-                            console.log(response3.error)
+                            setIsError(true)
+                            console.log("isError is " + isError)
 
                         } else {
 
@@ -193,21 +186,110 @@ function AdminPayments() {
                         }
 
                     } catch (error) {
-                        //setIsError(true);
+                        setIsError(true);
                         console.log(error);
                     
                     }
 
                 } else {
+                    setIsError(true);
                     console.log("Matching accounts not found")
                 }
 
             }
 
         } catch (error) {
-            //setIsError(true);
+            setIsError(true);
             console.log(error);
         
+        }
+
+    }
+
+    const handleSubmitExt = async (e) => {
+        e.preventDefault();
+
+        const ext_url = `http://localhost:8083/api/transfer/external/from/${formData.accountId3}/to/${formData.accountId4}`;
+
+        const url = `http://localhost:8083/api/getaccount/${formData.accountId3}`;
+
+        try {
+
+            const response4 = await fetch(url, {
+                method: 'GET',
+                headers: {
+                    'Access-Control-Allow-Origin': 'http://localhost:3000', 
+                    'Content-Type': 'application/json',  
+                },
+            })
+
+            if(!response4.ok){
+
+                setIsError(true)
+                console.log("isError is " + isError)
+   
+            } else {
+
+                const data4 = await response4.json();
+                console.log(data4)
+
+                if((parseInt(formData.sortCode3) === data4.sortCode)) {
+
+                    const doubleBal = Number(formData.amount2)
+                    console.log(doubleBal)
+
+                    const balObj = {
+                        balance: doubleBal,
+                        transLogDTO: {reference: formData.reference1}
+                    }
+
+                    console.log(balObj)
+
+                    try {
+
+                        const response5 = await fetch(ext_url, {
+                            method: 'PATCH',
+                            headers: {
+                                'Access-Control-Allow-Origin': 'http://localhost:3000', 
+                                'Content-Type': 'application/json',
+                            },
+
+                            body: JSON.stringify(balObj)
+
+                        });
+
+                        if(!response5.ok){
+
+                            setIsError(true)
+                            console.log("isError is " + isError)
+
+                        } else {
+
+                            const data5 = response5.json();
+
+                            console.log(data5)
+
+                            handleClearForm()
+
+                            setIsTransferSuccessfulExt(true)
+
+                        }
+
+                    } catch (error) {
+                        setIsError(true);
+                        console.log(error);
+                    
+                    }
+
+                } else {
+                    console.log("Matching account not found")
+                }
+            
+            }
+
+        } catch (error) {
+            setIsError(true);
+            console.log(error);
         }
 
     }
@@ -219,23 +301,54 @@ function AdminPayments() {
             amount: "",
             reference: "",
             accountId1: "",
-            sortCode1: ""
+            sortCode1: "",
+            //external
+            accountId3: "",
+            sortCode3: "",
+            accountId4: "",
+            sortCode4: "",
+            bank: "",
+            amount2: "",
+            reference1: ""
+
         });
     };
 
     useEffect(() => {
+
         if (isTransferSuccessful) {
+
             const timer = setTimeout(() => {
                 setIsTransferSuccessful(false);
+            }, 5000);
+
+            return () => {
+                clearTimeout(timer);
+            }
+
+        } else if (isTransferSuccessfulInt) {
+
+            const timer = setTimeout(() => {
                 setIsTransferSuccessfulInt(false);
             }, 5000);
 
             return () => {
                 clearTimeout(timer);
             }
-        }
-    }, [isTransferSuccessful]);
 
+        } else if (isTransferSuccessfulExt) {
+
+            const timer = setTimeout(() => {
+                setIsTransferSuccessfulExt(false);
+            }, 5000);
+
+            return () => {
+                clearTimeout(timer);
+            }
+
+        }
+
+    }, [isTransferSuccessful, isTransferSuccessfulInt, isTransferSuccessfulExt]);
 
   return (
     <div>
@@ -310,23 +423,23 @@ function AdminPayments() {
         
         </form>
 
-        <form>
+        <form onSubmit={handleSubmitExt}>
 
             <h2>Transfer funds to external account.</h2>
             
             <div>
 
-                <label htmlFor='accountId'><h4>From account:</h4></label>
-                <input type="number" name='accountId' value={formData.accountId} onChange={(e) => setFormData({ ...formData, accountId: e.target.value })}/>
+                <label htmlFor='accountId3'><h4>From account:</h4></label>
+                <input type="number" name='accountId3' value={formData.accountId3} onChange={(e) => setFormData({ ...formData, accountId3: e.target.value })}/>
 
-                <label htmlFor='sortCode'><h4>Sort Code:</h4></label>
-                <input type="number" name='sortCode' value={formData.sortCode} onChange={(e) => setFormData({ ...formData, sortCode: e.target.value })}/>
+                <label htmlFor='sortCode3'><h4>Sort Code:</h4></label>
+                <input type="number" name='sortCode3' value={formData.sortCode3} onChange={(e) => setFormData({ ...formData, sortCode3: e.target.value })}/>
 
-                <label htmlFor='accountId'><h4>To account:</h4></label>
-                <input type="number" name='accountId1'/>
+                <label htmlFor='accountId4'><h4>To account:</h4></label>
+                <input type="number" name='accountId4' value={formData.accountId4} onChange={(e) => setFormData({ ...formData, accountId4: e.target.value })}/>
 
-                <label htmlFor='sortCode'><h4>Sort Code:</h4></label>
-                <input type="number" name='sortCode1'/>
+                <label htmlFor='sortCode4'><h4>Sort Code:</h4></label>
+                <input type="number" name='sortCode4' value={formData.sortCode4} onChange={(e) => setFormData({ ...formData, sortCode4: e.target.value })}/>
 
                 <label htmlFor='bank'><h4>Bank Name:</h4></label>
                 <select name="bank">
@@ -339,14 +452,15 @@ function AdminPayments() {
                     <option value="santander">Santander</option>
                 </select>
 
-                <label htmlFor='amount'><h4>Amount:</h4></label>
-                <input type="number" name='amount' value={formData.amount} onChange={(e) => setFormData({ ...formData, amount: e.target.value })}/>
+                <label htmlFor='amount2'><h4>Amount:</h4></label>
+                <input type="number" name='amount2' value={formData.amount2} onChange={(e) => setFormData({ ...formData, amount2: e.target.value })}/>
 
-                <label htmlFor='reference'><h4>Reference:</h4></label>
-                <input type="text" name='reference' value={formData.reference} onChange={(e) => setFormData({ ...formData, reference: e.target.value })}/>
+                <label htmlFor='reference1'><h4>Reference:</h4></label>
+                <input type="text" name='reference1' value={formData.reference1} onChange={(e) => setFormData({ ...formData, reference1: e.target.value })}/>
                 <p></p>
+                <small>{isTransferSuccessfulExt && "Transfer was successful!"}</small>
                 <div>
-                    <button type='button'>Clear Form</button>
+                    <button type='button' onClick={handleClearForm}>Clear Form</button>
                
                     <button type='submit'>Confirm</button>
                 </div>
